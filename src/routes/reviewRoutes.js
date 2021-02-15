4ea2384094d3e8508c1f1aa901ad395837f115a4
karuifeather <aryalaashaya@gmail.com>
2021-02-15 16:00:09 +0545

    nested reviewRouter on tour route

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -3,7 +3,13 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
+
+// GET /tours/1431jh4523jk1/reviews
+// POST /tours/1431jh4523jk1/reviews
+// GET /tours/1431jh4523jk1/reviews/12g431jh3415g
+
+// /reviews
 
 router
   .route('/')
--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/tour-stats').get(tourController.getTourStats);
 
