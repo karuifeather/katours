594cefae06218dd13d45aaf311852d0aab8858c2
karuifeather <aryalaashaya@gmail.com>
2021-02-18 21:17:12 +0545

    added cors

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,9 @@ app.set('views', path.join(__dirname, 'views'));
 /**********************************
  * GLOBAL MIDDLEWARES
  * ********************************/
+// Implement cors
+app.use(cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -102,7 +106,7 @@ app.use(
   })
 );
 
-app.use(compression())
+app.use(compression());
 
 // app.get('/api/v1/tours', getAllTours);
 // app.get('/api/v1/tours/:id', getTour);
